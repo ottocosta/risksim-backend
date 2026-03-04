2035f9b9835b03b86e85ea68ce3e6575c19df374
ottocosta <ottoyliniemela@gmail.com>
2026-03-04 16:28:36 +0200

    Updated server.js to a complete working Express server.

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const routes = require('./src/routes/index');
+const errorMiddleware = require('./src/middleware/errorMiddleware');
+
 const app = express();
-
-app.get('/', (req, res) => {
-  res.send('RiskSim Backend is running!');
-});
-
 const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use('/api', routes);
+app.use(errorMiddleware);
+
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
